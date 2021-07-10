e6f2a991e1f2c8459943bfaa673a295edc2e728c
Basar <basar@workspaceit.com>
2021-07-10 06:57:22 +0600

    UI Dockerfile: added dynamic port func by envsubst and fixes nginx.conf

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8080/api',
-  apiLogoutUrl: 'http://localhost:8080/logout'
+  apiBaseUrl: 'https://social-media-spring.herokuapp.com/api',
+  apiLogoutUrl: 'https://social-media-spring.herokuapp.com/logout'
 };
